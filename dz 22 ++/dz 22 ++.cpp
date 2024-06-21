b7e1ae20934fd0b3803c6fe64a15b81596316175
kekmanzxc1 <stanislav61107@gmail.com>
2024-06-21 11:59:45 +0300

    Добавьте файлы проекта.

---
+++ dz 22 ++/dz 22 ++.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+class Point {
+private:
+    double x;
+    double y;
+    double z;
+
+public:
+    Point(double x, double y, double z) : x(x), y(y), z(z) {}
+
+    double getX() const {
+        return x;
+    }
+
+    double getY() const {
+        return y;
+    }
+
+    double getZ() const {
+        return z;
+    }
+};
+
+int main() {
+    Point p(1.0, 2.0, 3.0);
+    std::cout << "Coordinates x: " << p.getX() << std::endl;
+    std::cout << "Coordinates y: " << p.getY() << std::endl;
+    std::cout << "Coordinates z: " << p.getZ() << std::endl;
+}
